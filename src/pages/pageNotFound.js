5ce160ef9108da2aaebd2db46270b984f9309424
CornetS28 <corsam28@gmail.com>
2021-02-11 00:54:27 -0500

    Create the basic directoris, files, routes... to start

---
+++ src/pages/pageNotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <h1>The PageNotFound</h1>
+    </div>
+  );
+};
+export default PageNotFound;
---
+++ src/utils/theme.js
@@ -0,0 +1,29 @@
+export default {
+  palette: {
+    primary: {
+      main: "#000000",
+    },
+    secondary: {
+      main: "#F79905",
+    },
+    color: {
+      contrastWhite: "#cccccc",
+      white: "#ffffff",
+      darcGray: "#1C1C1C",
+      darkWhite: "#E4E5D1",
+    },
+
+    font: {
+      HelveticaNeue: "HelveticaNeue",
+      fontStretch: "normal",
+      lineHeight: 1.17,
+      letterSpacingBig: "1.23px",
+      letterSpacingSmall: "0.23px",
+      textAlign: "center",
+      bebasNeue: "Bebas Neue",
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+};
