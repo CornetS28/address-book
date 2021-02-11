5a2dea9573e441fa2c981fd6f1b6b0e004d74934
CornetS28 <corsam28@gmail.com>
2021-02-11 15:05:01 -0500

    Create the footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import { useHistory } from "react-router-dom";
 
-const Footer = () => {
+// Images
+// import logo from "../../images/logo.png";
+// import footerLogo from "../../images/logo.png";
+
+// MUI stuff
+import withStyles from "@material-ui/core/styles/withStyles";
+import Grid from "@material-ui/core/Grid";
+import Divider from "@material-ui/core/Divider";
+
+// Utils
+import styles from '../../utils/styles';
+
+const Footer = (props) => {
+  const history = useHistory();
+
+  const routeChange = (path) => {
+    history.push(path);
+  };
+  const { classes } = props;
+
   return (
-    <div>
-      <h1>The footer</h1>
-    </div>
-  )
-}
-export default Footer;
+    <>
+      <Divider className={classes.divider} />
+      <Grid spacing={10} className={classes.footerWrapper}>
+        <p className={classes.copyWriteText}>
+          Designed and developed by{" "}
+          <a
+            href="https://github.com/CornetS28"
+            target="_blank"
+            className={classes.copyWriteLink}
+          >
+            Samuel Cornet
+          </a>
+        </p>
+      </Grid>
+    </>
+  );
+};
+
+
+Footer.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Footer);
