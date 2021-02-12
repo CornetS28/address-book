a55abfe143499e364d9e57fabc186acbcdc82a87
CornetS28 <corsam28@gmail.com>
2021-02-11 21:40:58 -0500

    Create header page and filter

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,116 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-const Home = () => {
+// MUI stuff
+import withStyles from "@material-ui/core/styles/withStyles";
+import Grid from "@material-ui/core/Grid";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import TextField from "@material-ui/core/TextField";
+import Select from "@material-ui/core/Select";
+import NativeSelect from "@material-ui/core/NativeSelect";
+
+// Utils
+import styles from "../utils/styles";
+
+const signerOptions = [
+  {
+    value: "1",
+    label: "Broker/Manager",
+  },
+  {
+    value: "2",
+    label: "TBD 1",
+  },
+  {
+    value: "3",
+    label: "TBD 2",
+  },
+];
+
+const Home = ({ classes }) => {
+  const [signers, setSigners] = React.useState(1);
+
+  const onSignerChange = (event) => {
+    setSigners(event.target.value);
+  };
   return (
-    <div>
-      <h1>In the home page</h1>
-    </div>
-  )
-}
+    <Grid container className={classes.homePageWrapper}>
+      <Grid
+        container
+        item
+        xs={12}
+        sm={10}
+        className={classes.headerAndFilterWrapper}
+      >
+        <Grid item xs={12} sm={8}>
+          <Typography variant="h6" className={classes.header}>
+            Your contacts
+          </Typography>
+        </Grid>
 
-export default Home;
+        <Grid item xs={12} sm={4}>
+          <Grid container item sm={12} className={classes.filterContainer}>
+            <Grid item xs={4} sm={4} className={classes.sortBy}>
+              <p className={classes.sortByText}>Sort by</p>
+            </Grid>
+            <Grid item xs={8} sm={8}>
+              <Grid item xs={12} sm={12} className={classes.TextField2}>
+                <FormControl
+                  variant="outlined"
+                  className={classes.selecltFieldInput}
+                  size="small"
+                  fullWidth
+                  className={classes.formControl}
+                >
+                  <Select
+                    className={classes.select}
+                    labelId="demo-simple-select-filled-label"
+                    id="demo-simple-select-filled"
+                    value={signers}
+                    onChange={onSignerChange}
+                  >
+                    {signerOptions.map((option) => (
+                      <option
+                        key={option.label}
+                        value={option.value}
+                        style={{
+                          backgroundColor: "#000000",
+                          color: "yellow",
+                          padding: 5,
+                        }}
+                      >
+                        {option.label}
+                      </option>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={11} sm={12} style={{ margin: "auto" }}>
+          <Grid className={classes.smallEmpptySpace} />
+          <Divider className={classes.divider} />
+        </Grid>
+      </Grid>
+
+      <Grid
+        container
+        item
+        xs={12}
+        sm={10}
+        className={classes.contactsWrapper}
+      >ZCsdcdc</Grid>
+    </Grid>
+  );
+};
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Home);
