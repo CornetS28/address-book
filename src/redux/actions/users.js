ee4348c6989da03911550a1d80a71c00ffc6b3cf
CornetS28 <corsam28@gmail.com>
2021-02-16 02:58:27 -0500

    Hot fix

--- src/redux/actions/users.js
+++ src/redux/actions/users.js
@@ -4,7 +4,7 @@ import { Users } from './types';
 export const getUsersAction = () => async (dispatch) => {
   dispatch({ type: Users.GET_USER_START });
   return axios
-    .get('https://randomuser.me/api/?results=200')
+    .get('https://randomuser.me/api/?results=1000')
     .then((response) => {
       dispatch({
         type: Users.GET_USER_SUCCESS,
